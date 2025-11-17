4427becf3c8e9ac76d39f8920987f5b42904b372
Gonnij <giochiementin@gmail.com>
2025-11-17 15:33:26 +0100

    reverted __init__.py back to original

--- mask2former/mask2former/__init__.py
+++ mask2former/mask2former/__init__.py
@@ -1,13 +1,5 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
-# from . import data  # register all new datasets
-import warnings
-try:
-    from . import data  # register all new datasets
-except Exception as exc:  # pragma: no cover - best effort dataset registration
-    warnings.warn(
-        "[mask2former] Failed to register datasets (continuing without them): %s" % exc
-    )
-    data = None
+from . import data  # register all new datasets
 
 from . import modeling
 
